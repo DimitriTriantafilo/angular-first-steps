3d3e14778c193b4bd8cf0f86103adb839baaf119
Dimitri Triantafilo <dimitritriantafilo@gmail.com>
2021-08-03 14:49:07 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,45 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostComponent } from './post/post.component';
+import { PostEditComponent } from './post-edit/post-edit.component';
+import { AuthComponent } from './auth/auth.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'post-list',
+    pathMatch: 'full'
+  },
+  {
+    path: 'post-list',
+    component: PostListComponent
+  },
+  {
+    path: 'auth',
+    component: AuthComponent
+  },
+  {
+    path: 'post-add',
+    component: PostEditComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostListComponent,
+    PostComponent,
+    PostEditComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/post.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from "@angular/core";
+import { Post } from "./post.model";
+
+@Injectable({providedIn: 'root' })
+export class PostService{
+    listOfPosts: Post[] = [
+        new Post(
+          'Nature',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          'https://cdn.pixabay.com/photo/2015/06/19/21/24/avenue-815297__480.jpg',
+          'Dimi',
+          new Date()
+        ),
+        new Post(
+          'Desert',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          'https://api.timeforkids.com/wp-content/uploads/2017/08/170227012793_hero.jpg?w=1024',
+          'Dimi',
+          new Date()
+        ),
+        new Post(
+          'Mining',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          'https://perfilindustrial.com/wp-content/uploads/2020/02/miner%C3%ADa-subterranea.jpg',
+          'Dimi',
+          new Date()
+        ),
+      ];
+      getPosts(){
+          return this.listOfPosts
+      }
+      deletePost(index: number){
+          this.listOfPosts.splice(index, 1)
+      }
+      addPost(post: Post){
+          this.listOfPosts.push(post);
+      }
+      updatePost(index: number, post: Post){
+        this.listOfPosts[index] = post
+      }
+}
