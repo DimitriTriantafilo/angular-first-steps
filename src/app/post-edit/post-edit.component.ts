45097d6681a055a3729b12ad563890847032ca8c
Dimitri Triantafilo <dimitritriantafilo@gmail.com>
2021-08-04 20:07:02 -0300

    route and flags

--- src/app/post-edit/post-edit.component.ts
+++ src/app/post-edit/post-edit.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Post } from '../post.model';
 import { PostService } from '../post.service';
 
@@ -12,14 +12,35 @@ import { PostService } from '../post.service';
 })
 export class PostEditComponent implements OnInit {
   form!: FormGroup;
+  index: number = 0;
+  editMode = false;
 
-  constructor(private postService: PostService, private router: Router ) {}
+  constructor(private postService: PostService, private router: Router, private route: ActivatedRoute ) {}
 
   ngOnInit(): void {
+
+    let title = '';
+    let description = '';
+    let imagePath = '';
+
+    this.route.params.subscribe((params: Params) => {
+      if (params['index']){
+        console.log(params['index']);
+        this.index = params['index'];
+
+       const post = this.postService.getPost(this.index)
+        title = post.title;
+        description = post.description
+        imagePath = post.imagePath;
+
+        this.editMode = true;
+      }
+    })
+
     this.form = new FormGroup({
-      title: new FormControl(null, [Validators.required]),
-      description: new FormControl(null, [Validators.required]),
-      imagePath: new FormControl(null, [Validators.required]),
+      title: new FormControl(title, [Validators.required]),
+      description: new FormControl(description, [Validators.required]),
+      imagePath: new FormControl(imagePath, [Validators.required]),
     });
   }
 
@@ -33,9 +54,14 @@ export class PostEditComponent implements OnInit {
       description,
       imagePath,
       'test@test.com',
-      new Date()
+      new Date(),
+      0
     );
-    this.postService.addPost(post);
+    if(this.editMode){
+      this.postService.updatePost(this.index, post)
+    }
+    else{  this.postService.addPost(post);}
+   
     this.router.navigate(["/post-list"])
   }
 }
--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -9,21 +9,24 @@ export class PostService{
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
           'https://cdn.pixabay.com/photo/2015/06/19/21/24/avenue-815297__480.jpg',
           'Dimi',
-          new Date()
+          new Date(),
+          5
         ),
         new Post(
           'Desert',
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
           'https://api.timeforkids.com/wp-content/uploads/2017/08/170227012793_hero.jpg?w=1024',
           'Dimi',
-          new Date()
+          new Date(),
+          2
         ),
         new Post(
           'Mining',
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
           'https://perfilindustrial.com/wp-content/uploads/2020/02/miner%C3%ADa-subterranea.jpg',
           'Dimi',
-          new Date()
+          new Date(),
+          3
         ),
       ];
       getPosts(){
@@ -37,5 +40,8 @@ export class PostService{
       }
       updatePost(index: number, post: Post){
         this.listOfPosts[index] = post
+      }
+      getPost(index: number){
+        return this.listOfPosts[index]
       }
 }
